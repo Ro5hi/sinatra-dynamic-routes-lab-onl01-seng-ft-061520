ab66de4ac6d40868086762fc81f824319f3b02ed
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-14 20:20:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -36,6 +36,7 @@ end
       answer = (number1 + number2).to_s
       when "subtract"
         answer = (number1 - number2).to_s 
+        
         when "divide"
           answer = (number1 / number2).to_s 
           when "multiply"
