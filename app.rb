db248339320eace895bae1b70042bd4acb379a90
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-14 20:15:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -37,9 +37,9 @@ end
       when "subtract"
         answer = (number1 - number2).to_s 
         when "divide"
-          answer = (number1 / number2).to_s 
+          (number1 / number2).to_s 
           when "multiply"
-            answer = (number1 * number2).to_s
+            (number1 * number2).to_s
           end 
         end 
       end 
